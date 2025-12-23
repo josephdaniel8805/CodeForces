97b23c27db3b6d81549286f31a012d396b0f49d5
Joseph Daniel <josephdaniel8805@gmail.com>
2025-12-23 23:04:19 +0530

    Round-1071 Div-3 23rd Dec 2025 20:05 IST

---
+++ Round-1071/A.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int k=sc.nextInt(),x=sc.nextInt();
+            System.out.println((k*x)+1);
+        }
+    }
+}
---
+++ Round-1071/B.java
@@ -0,0 +1,29 @@
+import java.util.*;
+ 
+public class B {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int arr[]=new int[n];
+            ArrayList<Integer> list = new ArrayList<>();
+            int max=0,pos=0;
+            for(int ind=0;ind<n;ind++){
+                arr[ind]=sc.nextInt();
+            }
+            long total=0;
+            for(int ind=1;ind<n;ind++){
+                total+=Math.abs(arr[ind-1]-arr[ind]);
+            }
+            long ans=total;
+            ans=Math.min(ans,total-Math.abs(arr[0]-arr[1]));
+            ans=Math.min(ans,total-Math.abs(arr[n-1]-arr[n-2]));
+            for(int ind=1;ind<n-1;ind++){
+                long temp= total-Math.abs(arr[ind]-arr[ind-1])-Math.abs(arr[ind+1]-arr[ind])+Math.abs(arr[ind+1]-arr[ind-1]);
+                ans=Math.min(ans,temp);
+            }
+            System.out.println(ans);
+        }
+    }    
+}
